0aca201773ed34c5bcab32a0b85b4b8825a2ae61
zhimin.xi <zhimin.xi@pcc.edu>
2022-03-19 18:39:30 -0700

    add my code to index.html and use do while instead of while

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+let userName = window.prompt('Please enter your name');
+let userScore = 0;
+let playAgain = true;
 // Your Code Here
+function game(){
+     
+for(let i = 0; i < questions.length; i ++){
+    let question = questions[i];
+    let userAnswer = window.prompt(question.text); 
+    if(userAnswer == question.correctAnswer){
+        userScore = userScore + 10;
+    }
+ }
+
+ window.alert('Your Score is ' + userScore);
+}
+// while (playAgain ===true){
+//     game();
+//     let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.');
+//     if(userChoice ==='yes'){
+//         playAgain = true;
+//     }else{
+//         playAgain =false;
+//         window.alert('Thanks for playing the game !')
+//     }
+
+
+do{
+    game();
+    let userChoice = window.prompt('Would you like to play the game again? Answer yes or no.');
+    if (userChoice ==='yes')
+         playAgain = true; //It must be a boolen variable, don't use userChoice in while condition directly.
+    else{playAgain =false;
+        window.alert('Thanks for playing the game !')}
+    }while (playAgain)
+
